f7b1f5e355130b5ec5231ad7c401db1edbad2219
jnopareboateng <jnopareboateng@outlook.com>
2024-04-21 03:49:50 +0000

    Add password generation function to password_regex.py

---
+++ fcc_python/password_regex.py
@@ -0,0 +1,15 @@
+import secrets
+import string
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+    password = ''
+    # Generate password
+    for (i in range(0 to len(length))):
+        password+secrets.choice(all_characters)
