fbdf802ef178ce89c19cefabf836a24662a43ba7
jnopareboateng <jnopareboateng@outlook.com>
2024-04-27 05:45:18 +0000

    complete merge_sort function in password_regex.py

--- fcc_python/password_regex.py
+++ fcc_python/password_regex.py
@@ -1,40 +1,41 @@
-import re
-import secrets
-import string
-
-
-def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
-
-    # Define the possible characters for the password
-    letters = string.ascii_letters
-    digits = string.digits
-    symbols = string.punctuation
-
-    # Combine all characters
-    all_characters = letters + digits + symbols
-
-    while True:
-        password = ''
-        # Generate password
-        for _ in range(length):
-            password += secrets.choice(all_characters)
-        
-        constraints = [
-            (nums, r'\d'),
-            (special_chars, fr'[{symbols}]'),
-            (uppercase, r'[A-Z]'),
-            (lowercase, r'[a-z]')
-        ]
-
-        # Check constraints        
-        if all(
-            constraint <= len(re.findall(pattern, password))
-            for constraint, pattern in constraints
-        ):
-            break
+def merge_sort(array):
+    if len(array) <= 1:
+        return
     
-    return password
+    middle_point = len(array) // 2
+    left_part = array[:middle_point]
+    right_part = array[middle_point:]
+
+    merge_sort(left_part)
+    merge_sort(right_part)
+
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
+
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
     
-if __name__ =='__main__':
-    new_password = generate_password()
-    print('Generated password:', new_password)
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: '+ str(numbers))
