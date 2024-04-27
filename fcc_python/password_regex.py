f8a6e11651e3bcf057caf79366badff860399f0e
jnopareboateng <jnopareboateng@outlook.com>
2024-04-27 02:37:27 +0000

    update password_regex.py file

--- fcc_python/password_regex.py
+++ fcc_python/password_regex.py
@@ -3,7 +3,8 @@ import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -17,24 +18,23 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-       
+        
         constraints = [
             (nums, r'\d'),
-            (lowercase, r'[a-z]'),
-            (uppercase, r'[A-Z]'),            
-            (special_chars, fr'[{symbols}]')            
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
         ]
 
-        # Check constraints
-        count = 0
-        for constraint, pattern in constraints:
-            if constraint <= len(re.findall(pattern, password)):
-                count += 1
-            
-        if count == 4:
-            pass
-
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
     return password
-
-# new_password = generate_password(8)
-# print(new_password)
+    
+if __name__ =='__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
